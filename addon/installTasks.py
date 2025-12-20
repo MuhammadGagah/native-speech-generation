a512aa1af7dc5a0b78a27814fa6bffa5513b6cb4
Muhammad <muha.aku@gmail.com>
2025-12-21 01:16:48 +0700

    try fix again

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -11,23 +11,27 @@ addonHandler.initTranslation()
 
 CONFIG_DOMAIN = "NativeSpeechGeneration"
 
+
 def onInstall() -> None:
-    """
-    Called when the add-on is installed.
-    Dependency checks are now handled dynamically by the global plugin at runtime.
-    """
-    log.info("Native Speech Generation add-on installed. Dependencies will be checked periodically by the global plugin.")
+	"""
+	Called when the add-on is installed.
+	Dependency checks are now handled dynamically by the global plugin at runtime.
+	"""
+	log.info(
+		"Native Speech Generation add-on installed. Dependencies will be checked periodically by the global plugin.",
+	)
+
 
 def onUninstall() -> None:
-    """
-    Called when the add-on is uninstalled.
-    Cleans up configuration.
-    """
-    if CONFIG_DOMAIN in config.conf.spec:
-        del(config.conf.spec[CONFIG_DOMAIN])
-        
-    for profile in config.conf.profiles:
-        if CONFIG_DOMAIN in profile:
-            del(profile[CONFIG_DOMAIN])
-            
-    log.info(f"Configuration for '{CONFIG_DOMAIN}' has been cleaned up upon uninstallation.")
+	"""
+	Called when the add-on is uninstalled.
+	Cleans up configuration.
+	"""
+	if CONFIG_DOMAIN in config.conf.spec:
+		del config.conf.spec[CONFIG_DOMAIN]
+
+	for profile in config.conf.profiles:
+		if CONFIG_DOMAIN in profile:
+			del profile[CONFIG_DOMAIN]
+
+	log.info(f"Configuration for '{CONFIG_DOMAIN}' has been cleaned up upon uninstallation.")
